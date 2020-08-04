5fa56281e0b1735113391bebdef4f3f2d0c06bde
a <1115820867@qq.com>
2020-08-04 17:39:06 +0800

    Initial commit

---
+++ src/test/java/com/uwTech/pinpointtest/PinpointTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uwTech.pinpointtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinpointTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
